b6896ea4b924393cc544ff9d6c98893b7a4c4972
Jinme Mirabal <mirabalj@gmail.com>
2023-09-14 14:34:17 -0500

    Created middleware to catch errors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import 'dotenv/config';
 import { itemsRouter } from './items/items.router';
+import { errorHandler } from './middleware/error.middleware';
 
 require('dotenv').config();
 
@@ -17,6 +18,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/menu/items', itemsRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);

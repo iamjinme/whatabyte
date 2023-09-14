f9beca888f1cd3815af6c72a328c370023a1eb74
Jinme Mirabal <mirabalj@gmail.com>
2023-09-14 11:05:03 -0500

    Minimal server with Typescript

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import 'dotenv/config';
+
+require('dotenv').config();
+
+if (!process.env.PORT) process.exit(1);
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
